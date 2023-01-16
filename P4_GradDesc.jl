8126d3716e7ee9abe610fe35871ee52447597d20
UdcsPa <122739957+UdcsPa@users.noreply.github.com>
2023-01-16 09:27:17 +0530

    Update P4_GradDesc.jl

--- P4_GradDesc.jl
+++ P4_GradDesc.jl
@@ -6,6 +6,7 @@ function gradient_descent(P, q, x₀; α = 0.1, maxitter = 1000, ϵ = 1e-5)
     x = copy(x₀)
     ▽f = x -> P * x + q
     △x = -▽f(x)
+    iter = 0
     while norm(△x) > ϵ || iter <= maxitter
         iter += 1
         x .+= ϵ * △x
